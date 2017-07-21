5de7965c8cdd405a36363ca9173fc6acb0099e30
VCBackup <dmoore1525@gmail.com>
2017-07-21 16:03:51 -0600

    Completed assignment.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {Provider} from 'react-redux';
 import store from './store';
 import App from './components/App/App';
 import Login from './components/Login/Login';
+import Shop from './components/Shop/Shop';
 
 ReactDOM.render(
     <Provider store={store}>
@@ -13,6 +14,8 @@ ReactDOM.render(
             <Route path='/' Component={App}>
             </Route>
             <Route path='/login' Component={Login}>
+            </Route>
+            <Route path='/shop' Component={Shop}>
             </Route>
         </Router>
 </Provider>,
